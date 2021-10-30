1d2755800d795829a695c7c2c3c40fc77542477d
cauegraciolip <cauegracioli@outlook.com>
2021-10-30 19:03:45 -0300

    fatoração do código para code review

--- exercicio-sprint-03/checker-functional/checkerInFunctional.js
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.js
@@ -1,9 +1,9 @@
 "use strict";
-var inputArray = [10, 300, 45, 21, 634, 71, 1, 22, 3];
+var inputArray = [1, 5, 10];
 function checkMaxAndMin(arr) {
-    var reduced = arr.reduce(function (acc, next) { return acc + next; });
-    var media = reduced / arr.length;
-    var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), media];
+    var reduced = arr.reduce(function (acc, next) { return acc + next; }) / arr.length;
+    var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), reduced];
+    console.log(output);
     return output;
 }
 checkMaxAndMin(inputArray);
--- exercicio-sprint-03/checker-functional/checkerInFunctional.ts
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.ts
@@ -1,12 +1,14 @@
-const inputArray : number[] = [10, 300, 45, 21, 634, 71, 1, 22, 3]
+const inputArray: Array<number>= [1, 5, 10];
 
-function checkMaxAndMin(arr : number[]) : number[] {
-    const reduced : number = arr.reduce((acc, next) => acc + next);
-    const media : number = reduced / arr.length
-    const output = [Math.max(...arr), Math.min(...arr), media]
-    return output
+function checkMaxAndMin(arr: Array<number>): number[]{
+    const reduced: number = arr.reduce((acc, next) => acc + next) / arr.length;
+    const output : Array<number> = [Math.max(...arr), Math.min(...arr), reduced];
+    console.log(output)
+    return output;
 }
 
 checkMaxAndMin(inputArray)
+
+
 
 
