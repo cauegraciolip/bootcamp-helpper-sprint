41c8357d415837bec4966452f1bacecf50b7aaf2
cauegraciolip <cauegracioli@outlook.com>
2021-10-30 19:40:27 -0300

    agora sim adicionei os comentarios

--- exercicio-sprint-03/checker-functional/checkerInFunctional.js
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.js
@@ -1,9 +1,10 @@
 "use strict";
 var inputArray = [1, 5, 10];
 function checkMaxAndMin(arr) {
+    // high order function para somar todos os valore do array
     var reduced = arr.reduce(function (acc, next) { return acc + next; }) / arr.length;
+    // retorno do array já com valores de menor e maior
     var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), reduced];
-    console.log(output);
     return output;
 }
 checkMaxAndMin(inputArray);
--- exercicio-sprint-03/checker-functional/checkerInFunctional.ts
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.ts
@@ -1,9 +1,11 @@
 const inputArray: Array<number>= [1, 5, 10];
 
 function checkMaxAndMin(arr: Array<number>): number[]{
+    // high order function para somar todos os valore do array
     const reduced: number = arr.reduce((acc, next) => acc + next) / arr.length;
+    // retorno do array já com valores de menor e maior
     const output : Array<number> = [Math.max(...arr), Math.min(...arr), reduced];
-    console.log(output)
+
     return output;
 }
 
