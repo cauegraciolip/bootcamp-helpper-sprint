1d6331ef004fe23faf2b80fa95a2a16c8f70486e
cauegraciolip <cauegracioli@outlook.com>
2021-11-01 21:58:14 -0300

    refatoracao para mostrar em tela o resultado da conta de vogais

--- exercicio-sprint-01/item-A/countVolgals.js
+++ exercicio-sprint-01/item-A/countVolgals.js
@@ -1,9 +1,13 @@
 "use strict";
+let parameterDiv = document.querySelector('#parameter-div');
+let parameterText = document.querySelector('#text-parameter');
 // função para contar vogais a partir de parametros
 function countVowelsParams(word) {
     //  separa em um array somente as vogais
-    const arrVowels = word.match(/['aeiou]/gi);
-    // retorna a quantidade de itens dentro desse array
-    return arrVowels.length;
+    const arrVowels = word.match(/['aeiouéáàíóúê']/gi);
+    // mostra a palavra passado pelo parametro
+    parameterText.innerHTML = word.toString();
+    // coloca o length de arrVowels na div parameter-div
+    return parameterDiv.innerHTML = arrVowels.length.toString();
 }
-console.log(`O parametro possui ${countVowelsParams("Teste de funcionamento do código")} vogais.`);
+countVowelsParams('paralelepipedo');
--- exercicio-sprint-01/item-A/countVolgals.ts
+++ exercicio-sprint-01/item-A/countVolgals.ts
@@ -1,9 +1,15 @@
+let parameterDiv : HTMLDivElement = document.querySelector('#parameter-div') as HTMLDivElement
+let parameterText : HTMLDivElement = document.querySelector('#text-parameter') as HTMLDivElement
+
+
 // função para contar vogais a partir de parametros
-function countVowelsParams (word: String): number {
+function countVowelsParams (word: String): string {
      //  separa em um array somente as vogais
-     const arrVowels: RegExpMatchArray = word.match(/['aeiou]/gi) as RegExpMatchArray
-     // retorna a quantidade de itens dentro desse array
-     return arrVowels.length
+     const arrVowels: RegExpMatchArray = word.match(/['aeiouéáàíóúê']/gi) as RegExpMatchArray
+     // mostra a palavra passado pelo parametro
+     parameterText.innerHTML = word.toString()
+     // coloca o length de arrVowels na div parameter-div
+     return parameterDiv.innerHTML = arrVowels.length.toString()
 }
 
-console.log(`O parametro possui ${countVowelsParams("Teste de funcionamento do código")} vogais.`)
+countVowelsParams('paralelepipedo')
