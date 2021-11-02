338c26245124532ee7e27ee1d8dc52ec644119f4
cauegraciolip <cauegracioli@outlook.com>
2021-11-01 22:32:25 -0300

    add variaveis HTML para receber os valores a serem mostrados - ex 3 funcional

--- exercicio-sprint-03/checker-functional/checkerInFunctional.js
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.js
@@ -1,10 +1,18 @@
 "use strict";
-var inputArray = [1, 5, 10];
+var inputArray = [1, 5, 10, 335, 1000];
+var maxDiv = document.querySelector('#receive-max');
+var minDiv = document.querySelector('#receive-min');
+var mediaDiv = document.querySelector('#receive-media');
+var listDiv = document.querySelector('#lista');
+listDiv.innerHTML = inputArray.toString();
 function checkMaxAndMin(arr) {
     // high order function para somar todos os valore do array
     var reduced = arr.reduce(function (acc, next) { return acc + next; }) / arr.length;
     // retorno do array já com valores de menor e maior
     var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), reduced];
+    maxDiv.innerHTML = Math.max.apply(Math, arr).toString();
+    minDiv.innerHTML = Math.min.apply(Math, arr).toString();
+    mediaDiv.innerHTML = reduced.toString();
     return output;
 }
 checkMaxAndMin(inputArray);
--- exercicio-sprint-03/checker-functional/checkerInFunctional.ts
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.ts
@@ -1,13 +1,23 @@
-const inputArray: Array<number>= [1, 5, 10];
+const inputArray: Array<number> = [1, 5, 10, 335, 1000];
 
+let maxDiv : HTMLDivElement = document.querySelector('#receive-max') as HTMLDivElement;
+let minDiv : HTMLDivElement = document.querySelector('#receive-min') as HTMLDivElement;
+let mediaDiv : HTMLDivElement = document.querySelector('#receive-media') as HTMLDivElement;
+let listDiv : HTMLDivElement = document.querySelector('#lista') as HTMLDivElement;
+
+listDiv.innerHTML = inputArray.toString();
 function checkMaxAndMin(arr: Array<number>): number[]{
     // high order function para somar todos os valore do array
     const reduced: number = arr.reduce((acc, next) => acc + next) / arr.length;
     // retorno do array já com valores de menor e maior
     const output : Array<number> = [Math.max(...arr), Math.min(...arr), reduced];
 
+    maxDiv.innerHTML = Math.max(...arr).toString();
+    minDiv.innerHTML = Math.min(...arr).toString();
+    mediaDiv.innerHTML = reduced.toString();
+
     return output;
 }
 
-checkMaxAndMin(inputArray)
+checkMaxAndMin(inputArray);
 
