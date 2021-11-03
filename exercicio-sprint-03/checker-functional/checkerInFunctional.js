aa8e7d73858b747dd617f912a1c480c851399147
cauegraciolip <cauegracioli@outlook.com>
2021-11-03 13:11:40 -0300

    adicao de condicional para verificar se o array possui string - funcional

--- exercicio-sprint-03/checker-functional/checkerInFunctional.js
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.js
@@ -1,18 +1,31 @@
 "use strict";
-var inputArray = [1, 5, 10, 335, 1000];
+var inputArray = ['1', '5', "10", "335", "1000"];
 var maxDiv = document.querySelector('#receive-max');
 var minDiv = document.querySelector('#receive-min');
 var mediaDiv = document.querySelector('#receive-media');
 var listDiv = document.querySelector('#lista');
 listDiv.innerHTML = inputArray.toString();
+function isANumber(arr) {
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var i = arr_1[_i];
+        if (isNaN(arr[i])) {
+            console.log('Existe letras nesse array, faça com que seja somente números.');
+            return false;
+        }
+    }
+    return true;
+}
 function checkMaxAndMin(arr) {
-    // high order function para somar todos os valore do array
-    var reduced = arr.reduce(function (acc, next) { return acc + next; }) / arr.length;
-    // retorno do array já com valores de menor e maior
-    var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), reduced];
-    maxDiv.innerHTML = Math.max.apply(Math, arr).toString();
-    minDiv.innerHTML = Math.min.apply(Math, arr).toString();
-    mediaDiv.innerHTML = reduced.toString();
-    return output;
+    if (isANumber(arr)) {
+        // high order function para somar todos os valore do array
+        var reduced = arr.reduce(function (acc, next) { return acc + next; }) / arr.length;
+        // retorno do array já com valores de menor e maior
+        var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), reduced];
+        maxDiv.innerHTML = Math.max.apply(Math, arr).toString();
+        minDiv.innerHTML = Math.min.apply(Math, arr).toString();
+        mediaDiv.innerHTML = reduced.toString();
+        return output;
+    }
+    return [];
 }
 checkMaxAndMin(inputArray);
--- exercicio-sprint-03/checker-functional/checkerInFunctional.ts
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.ts
@@ -1,4 +1,4 @@
-const inputArray: Array<number> = [1, 5, 10, 335, 1000];
+const inputArray: Array<any> | Array<number> = ['1', '5', "10", "335", "1000"];
 
 let maxDiv : HTMLDivElement = document.querySelector('#receive-max') as HTMLDivElement;
 let minDiv : HTMLDivElement = document.querySelector('#receive-min') as HTMLDivElement;
@@ -6,17 +6,34 @@ let mediaDiv : HTMLDivElement = document.querySelector('#receive-media') as HTML
 let listDiv : HTMLDivElement = document.querySelector('#lista') as HTMLDivElement;
 
 listDiv.innerHTML = inputArray.toString();
+
+function isANumber(arr: Array<number>) : boolean{
+    for(let i of arr) {
+        if(isNaN(arr[i])) {
+            console.log('Existe letras nesse array, faça com que seja somente números.')
+            return false
+        }
+    }
+
+    return true
+}
 function checkMaxAndMin(arr: Array<number>): number[]{
-    // high order function para somar todos os valore do array
-    const reduced: number = arr.reduce((acc, next) => acc + next) / arr.length;
-    // retorno do array já com valores de menor e maior
-    const output : Array<number> = [Math.max(...arr), Math.min(...arr), reduced];
+    if(isANumber(arr)) {
+        // high order function para somar todos os valore do array
+        const reduced: number = arr.reduce((acc, next) => acc + next) / arr.length;
+        // retorno do array já com valores de menor e maior
+        const output : Array<number> = [Math.max(...arr), Math.min(...arr), reduced];
 
-    maxDiv.innerHTML = Math.max(...arr).toString();
-    minDiv.innerHTML = Math.min(...arr).toString();
-    mediaDiv.innerHTML = reduced.toString();
+        maxDiv.innerHTML = Math.max(...arr).toString();
+        minDiv.innerHTML = Math.min(...arr).toString();
+        mediaDiv.innerHTML = reduced.toString();
 
-    return output;
+        return output;
+
+    }
+
+    return []
+    
 }
 
 checkMaxAndMin(inputArray);
