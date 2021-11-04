ac0f0bc5b3212f35c95dc85f00c4b06550bb07ac
cauegraciolip <cauegracioli@outlook.com>
2021-11-03 23:14:42 -0300

    adicao de condicional de verificacao -- imperativo e funcional

--- exercicio-sprint-03/checker-functional/checkerInFunctional.js
+++ exercicio-sprint-03/checker-functional/checkerInFunctional.js
@@ -1,13 +1,12 @@
 "use strict";
-var inputArray = ['1', '5', "10", "335", "1000"];
-var maxDiv = document.querySelector('#receive-max');
-var minDiv = document.querySelector('#receive-min');
-var mediaDiv = document.querySelector('#receive-media');
-var listDiv = document.querySelector('#lista');
+const inputArray = ['1', '5', "10", "335", "1000"];
+let maxDiv = document.querySelector('#receive-max');
+let minDiv = document.querySelector('#receive-min');
+let mediaDiv = document.querySelector('#receive-media');
+let listDiv = document.querySelector('#lista');
 listDiv.innerHTML = inputArray.toString();
 function isANumber(arr) {
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var i = arr_1[_i];
+    for (let i of arr) {
         if (isNaN(arr[i])) {
             console.log('Existe letras nesse array, faça com que seja somente números.');
             return false;
@@ -18,11 +17,11 @@ function isANumber(arr) {
 function checkMaxAndMin(arr) {
     if (isANumber(arr)) {
         // high order function para somar todos os valore do array
-        var reduced = arr.reduce(function (acc, next) { return acc + next; }) / arr.length;
+        const reduced = arr.reduce((acc, next) => acc + next) / arr.length;
         // retorno do array já com valores de menor e maior
-        var output = [Math.max.apply(Math, arr), Math.min.apply(Math, arr), reduced];
-        maxDiv.innerHTML = Math.max.apply(Math, arr).toString();
-        minDiv.innerHTML = Math.min.apply(Math, arr).toString();
+        const output = [Math.max(...arr), Math.min(...arr), reduced];
+        maxDiv.innerHTML = Math.max(...arr).toString();
+        minDiv.innerHTML = Math.min(...arr).toString();
         mediaDiv.innerHTML = reduced.toString();
         return output;
     }
--- exercicio-sprint-03/checker-imperative/checkerInImperative.js
+++ exercicio-sprint-03/checker-imperative/checkerInImperative.js
@@ -1,14 +1,14 @@
 "use strict";
-var input = [10, 9, 4, 3, 7, 12, 3, 4];
-var majorDiv = document.querySelector('#receive-max-imp');
-var minorDiv = document.querySelector('#receive-min-imp');
-var mdDiv = document.querySelector('#receive-media-imp');
-var listaDiv = document.querySelector('#lista-imp');
-var maior = input[0];
-var menor = input[0];
-var acc = 0;
+const input = [10, 9, 4, 3, 7, 12, 3, 4];
+let majorDiv = document.querySelector('#receive-max-imp');
+let minorDiv = document.querySelector('#receive-min-imp');
+let mdDiv = document.querySelector('#receive-media-imp');
+let listaDiv = document.querySelector('#lista-imp');
+let maior = input[0];
+let menor = input[0];
+let acc = 0;
 function checkIsNumber(arr) {
-    for (var i in arr) {
+    for (let i in arr) {
         if (isNaN(arr[i])) {
             console.log('Existem letras ou string nesse array, faça com que seja somente números.');
             return false;
@@ -16,10 +16,9 @@ function checkIsNumber(arr) {
     }
     return true;
 }
-var findMinorMajorAndMedia = function (arr) {
+const findMinorMajorAndMedia = (arr) => {
     if (checkIsNumber(arr)) {
-        for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-            var index = arr_1[_i];
+        for (let index of arr) {
             // acumula os valores dentro de acc;
             acc += index;
             // confere se o index é menor ou maior que os atribuidos nas variaveis e atribui um novo valor caso true;
@@ -35,7 +34,7 @@ var findMinorMajorAndMedia = function (arr) {
         }
         ;
         // media do acumulado
-        var media = acc / arr.length;
+        const media = acc / arr.length;
         mdDiv.innerHTML = media.toString();
         return [maior, menor, media];
     }
