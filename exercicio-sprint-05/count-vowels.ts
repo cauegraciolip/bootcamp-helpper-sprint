02d4b36e44db6c9e3f249fe0187d992c0e66b68b
cauegraciolip <cauegracioli@outlook.com>
2021-11-07 19:06:14 -0300

    alteracoes pr --- delete getter e setter e adicionado metodos, adicionado condicao de input sem vogais

--- exercicio-sprint-05/count-vowels.ts
+++ exercicio-sprint-05/count-vowels.ts
@@ -1,37 +1,48 @@
-const input = document.querySelector("#input-text") as HTMLInputElement;
-const btn = document.querySelector('#btn') as HTMLButtonElement;
-const div = document.querySelector('#div') as HTMLDivElement;
+const input : HTMLInputElement = document.querySelector("#input-text") as HTMLInputElement;
+const btn : HTMLButtonElement = document.querySelector('#btn') as HTMLButtonElement;
+const div : HTMLDivElement = document.querySelector('#div') as HTMLDivElement;
 
 btn.addEventListener('click', () => {
-    const arrVowelsInput = letters.counter;
-    return div.innerHTML = `Seu input possui ${arrVowelsInput.toString()} vogal(ais).`;
+    const arrVowelsInput : number = letters.counter();
+    return div.innerHTML = `Seu texto possui ${arrVowelsInput.toString()} vogal(ais).`;
 })
 
 class Vowels { 
     private _inputValue: string;
     constructor() {
         this._inputValue = "";
-    }
+    };
 
-    get gVowels() : RegExpMatchArray {
-       return this._inputValue.match(/["aeiouéíóúìàáê"]/gi) as RegExpMatchArray;
-    }
+    /**
+     * Método cria array somente com vogais e retorna número vogais.
+     * @returns retorna quantidade de vogais
+     */
+    public counter() : number {
+        const getVowels : RegExpMatchArray = this._inputValue.match(/["aeiouéíóúìàáê"]/gi) as RegExpMatchArray;
+        if(getVowels === null) {
+            return 0;
+        };
+        return getVowels.length;
+    };
 
-    get counter() {
-        return this.gVowels.length;
-    }
+    /**
+     * Método que recebe valor do input e atribui a inputValue.
+     * @param input recebido do evento do eventlistener
+     * @returns reatribuição de _inputValue 
+     */
+    public valueOfInput(input : string) : string {
+        return this._inputValue = input;
+    };
+};
 
-    set LiveValue(input : string) {
-        this._inputValue = input;
-    }
-}
+const letters : Vowels = new Vowels();
 
-const letters = new Vowels();
-
-input.addEventListener('input', (event : Event) => {
-    const lettersOfInput = event.currentTarget as HTMLInputElement;
-    return letters.LiveValue = lettersOfInput.value;
-})
-
-
-
+/**
+ * adiciona evento ao input
+ * @event input evento captura escrita do input
+ * @return retorna string com valor do input;
+ */
+input.addEventListener('input', (event : Event) : string => {
+    const lettersOfInput : HTMLInputElement = event.currentTarget as HTMLInputElement;
+    return letters.valueOfInput(lettersOfInput.value);
+});
--- exercicio-sprint-05/dist/count-vowels.js
+++ exercicio-sprint-05/dist/count-vowels.js
@@ -3,38 +3,46 @@ var input = document.querySelector("#input-text");
 var btn = document.querySelector('#btn');
 var div = document.querySelector('#div');
 btn.addEventListener('click', function () {
-    var arrVowelsInput = letters.counter;
-    return div.innerHTML = "Seu input possui " + arrVowelsInput.toString() + " vogal(ais).";
+    var arrVowelsInput = letters.counter();
+    return div.innerHTML = "Seu texto possui " + arrVowelsInput.toString() + " vogal(ais).";
 });
 var Vowels = /** @class */ (function () {
     function Vowels() {
         this._inputValue = "";
     }
-    Object.defineProperty(Vowels.prototype, "gVowels", {
-        get: function () {
-            return this._inputValue.match(/["aeiouéíóúìàáê"]/gi);
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Vowels.prototype, "counter", {
-        get: function () {
-            return this.gVowels.length;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Vowels.prototype, "LiveValue", {
-        set: function (input) {
-            this._inputValue = input;
-        },
-        enumerable: false,
-        configurable: true
-    });
+    ;
+    /**
+     * Método cria array somente com vogais e retorna número vogais.
+     * @returns retorna quantidade de vogais
+     */
+    Vowels.prototype.counter = function () {
+        var getVowels = this._inputValue.match(/["aeiouéíóúìàáê"]/gi);
+        if (getVowels === null) {
+            return 0;
+        }
+        ;
+        return getVowels.length;
+    };
+    ;
+    /**
+     * Método que recebe valor do input e atribui a inputValue.
+     * @param input recebido do evento do eventlistener
+     * @returns reatribuição de _inputValue
+     */
+    Vowels.prototype.valueOfInput = function (input) {
+        return this._inputValue = input;
+    };
+    ;
     return Vowels;
 }());
+;
 var letters = new Vowels();
+/**
+ * adiciona evento ao input
+ * @event input evento captura escrita do input
+ * @return retorna string com valor do input;
+ */
 input.addEventListener('input', function (event) {
     var lettersOfInput = event.currentTarget;
-    return letters.LiveValue = lettersOfInput.value;
+    return letters.valueOfInput(lettersOfInput.value);
 });
