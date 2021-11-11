e79261d1a739aa18a0ad04c84530d8fa779ad45f
cauegraciolip <79729432+cauegraciolip@users.noreply.github.com>
2021-11-10 23:28:27 -0300

    Delete exercicio-sprint-05 directory

--- exercicio-sprint-05/compiled/Person.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PersonFactory = void 0;
-class PersonFactory {
-}
-exports.PersonFactory = PersonFactory;
-;
--- exercicio-sprint-05/src/cliente-code.ts
+++
@@ -1,7 +0,0 @@
-import { ProgrammerFactory } from "./ProgrammerFactory";
-
-const programmerFactory : ProgrammerFactory = new ProgrammerFactory();
-
-const ada = programmerFactory.getPerson(1, "ada", "programadora")
-
-console.log(ada);
